81c04ab8826a0841162d56e7b8ef6d9a43fa83f5
Yegis <tsiu.egor@yandex.ua>
2017-10-29 16:47:14 +0100

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -51,7 +51,7 @@ namespace TestBot
         Telegram.Bot.Types.Message msg = messageEventArgs.Message;
 			if (msg == null || msg.Type != MessageType.TextMessage) return;
 
-            var dict = new Dictionary<DateTime, List<string>>();
+            /*   var dict = new Dictionary<DateTime, List<string>>();
             String Answer = "";
 
             switch (msg.Text)
@@ -64,15 +64,15 @@ namespace TestBot
             date = Convert.ToDateTime(fast);
             name.Reverse();
             db.Method(dict, name, date);
-
-            /*    switch (msg.Text){
+*/
+             switch (msg.Text){
                      case "/start": Answer = "/stone - твой камень\r\n/scissors - твои ножницы\r\n/paper - твоя бумага\r\n/baba - показать бабу";break;
                      case "/stone": Answer = "А у меня бумага - ты проиграл"; break;
                      case "/scissors": Answer = "А у меня камень - ты проиграл"; break;
                      case "/paper": Answer = "А у меня ножницы - ты проиграл"; break;
                      case "/baba": Answer = "Вот тебе баба -http://sandi-rzn.ru/wp-content/uploads/c5b6dac3b02717e7fd6781b7515d64b4.jpg"; break;
                      default: Answer = "иди нахуй!"; break;
-                }*/
+                }
             await BOT.SendTextMessageAsync(msg.Chat.Id,Answer);	
 			
 		}
@@ -81,9 +81,9 @@ namespace TestBot
 }
     public class DB
     {
-        public List<string> celebr { get; set; }
+       // public List<string> celebr { get; set; }
 
-        public DateTime dtime { get; set; }
+    //    public DateTime dtime { get; set; }
         public void Method(Dictionary<DateTime, List<string>> obj, List<string> Name, DateTime date)
         {
             obj.Add(new DateTime(date.Year, date.Month, date.Day), new List<string>(Name));// { "new year", "buhastus" });
